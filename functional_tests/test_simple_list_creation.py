01365fd0e9a21e991078197fc9321776a2a68d52
davidsmith3 <david.smith3@outlook.com>
2015-02-15 17:50:20 -0500

    refactored ft

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,38 +1,7 @@
-import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from unittest import skip
+from .base import FunctionalTest
 
-
-class FunctionalTest(StaticLiveServerTestCase):
-
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.split('=')[1]
-				return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
-
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
-
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-
-	def tearDown(self):
-		self.browser.refresh()
-		self.browser.quit()
-
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
 
 class NewVisitorTest(FunctionalTest):
 
@@ -77,33 +46,6 @@ class NewVisitorTest(FunctionalTest):
 		page_text = self.browser.find_element_by_tag_name('body').text 
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
-
-class LayoutAndStylingTest(FunctionalTest):
-
-	def test_layout_and_styling(self):
-		self.browser.get(self.server_url)
-		self.browser.set_window_size(1024, 768)
-
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta = 10
-		)
-
-		inputbox.send_keys('testing\n')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta = 10
-		)
-
-class ItemValidationTest(FunctionalTest):
-
-	@skip
-	def test_cannot_add_empty_list_items(self):
-		self.fail('write me')
 
 
 
